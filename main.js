0c767fddab83779631ff27c14a70e9bb1401f8ce
Robyn Smith <rtasmith05@gmail.com>
2016-04-23 14:04:02 -0400

    User story #1 finished.

--- main.js
+++ main.js
@@ -1,14 +1,21 @@
-/* this is the format JSON is returned from:
-
-{"quoteText":"A house divided against itself cannot stand.", "quoteAuthor":"Abraham Lincoln", "senderName":"", "senderLink":"", "quoteLink":"http://forismatic.com/en/25d6485021/"}
-
-*/
-
 var gr;
+
+function displayContent(data) {
+  //console.log(data);
+  if (data["stream"] === null) {
+    $(".message").html("freecodecamp: offline");
+  }
+  else {
+    $(".message").html("freecodecamp: online");
+  }
+
+}
 
 $(document).ready(function() {
     $.getJSON('https://api.twitch.tv/kraken/streams/freecodecamp?callback=?', function(data) {
-      console.log(data);
       gr = data;
+
+      displayContent(data);
+
     });
 });
